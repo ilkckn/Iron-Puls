14355bade76e1d42fb9eec055b6f37fa0d33c541
Musa Cekcen <cekcen14@gmail.com>
2026-06-05 19:28:24 +0200

    navbar blur test5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    transformer: "postcss", // Lightning CSS'i kapat, postcss kullan
+    lightningcss: false,
+  },
+});
